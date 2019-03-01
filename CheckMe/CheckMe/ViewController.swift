f083fa7d1069e0c119689dcf63eb6ea46a0e64c8
VadimSos <vadimsos@icloud.com>
2019-03-01 08:32:53 +0300

    1) install CocoaPods/swiftlint, 2) update swiftlint rules

---
+++ CheckMe/CheckMe/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CheckMe
+//
+//  Created by Vadzim Sasnouski on 2/28/19.
+//  Copyright © 2019 Vadzim Sasnouski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
