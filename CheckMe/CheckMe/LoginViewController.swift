9398e63e3bef4158b390d2d94e2252d1324653f8
VadimSos <vadimsos@icloud.com>
2019-03-05 20:29:41 +0300

    create login storyboard view

---
+++ CheckMe/CheckMe/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  CheckMe
+//
+//  Created by Vadzim Sasnouski on 3/5/19.
+//  Copyright © 2019 Vadzim Sasnouski. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
