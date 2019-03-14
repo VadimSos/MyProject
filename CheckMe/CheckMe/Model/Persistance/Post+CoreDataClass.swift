7056f0bd25d7c45234afd617808c71411a31b9e1
VadimSos <vadimsos@icloud.com>
2019-03-14 20:20:31 +0300

    add CoreData model

---
+++ CheckMe/CheckMe/Model/Persistance/Post+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Post+CoreDataClass.swift
+//  
+//
+//  Created by Vadzim Sasnouski on 3/14/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Post)
+public class Post: NSManagedObject {
+
+}
---
+++ CheckMe/CheckMe/Model/Persistance/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  
+//
+//  Created by Vadzim Sasnouski on 3/14/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
