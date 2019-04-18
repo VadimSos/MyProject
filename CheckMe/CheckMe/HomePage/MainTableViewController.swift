d82bddc17d9c9ef7e4746c8e0aeb9c8f454c4656
VadimSos <vadimsos@icloud.com>
2019-04-18 22:46:23 +0300

    add 'DZNEmptyDataSet' to home screen

--- CheckMe/CheckMe/HomePage/MainTableViewController.swift
+++ CheckMe/CheckMe/HomePage/MainTableViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import Firebase
+import DZNEmptyDataSet
 
 class MainTableViewController: UIViewController {
 
@@ -25,6 +26,7 @@ class MainTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		displayPostsMainVC()
+		configureView()
     }
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -94,5 +96,57 @@ extension MainTableViewController: UITableViewDataSource {
 		cell.updateTableView(with: postsArray[indexPath.row])
 
 		return cell
+	}
+
+	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		// Return false if do not want the specified item to be editable
+		return true
+	}
+
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			postsArray.remove(at: indexPath.row)
+			tableView.deleteRows(at: [IndexPath](), with: .fade)
+			self.tableView.reloadEmptyDataSet()
+		}
+	}
+}
+
+// MARK: - DZNEmptyDataSetSource, DZNEmptyDataSetDelegate
+
+extension MainTableViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+
+	func configureView() {
+		// Update the user interface for the detail item.
+		tableView.emptyDataSetSource = self
+		tableView.emptyDataSetDelegate = self
+
+		// Removing the cell separators
+		tableView.tableFooterView = UIView()
+	}
+
+//	func customView(forEmptyDataSet scrollView: UIScrollView!) -> UIView! {
+//		let view = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+//		return view
+//	}
+
+	func image(forEmptyDataSet scrollView: UIScrollView!) -> UIImage! {
+		return UIImage(named: "EmptyPage")
+	}
+
+	func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+		let str = "No Posts"
+		let attrs = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)]
+		return NSAttributedString(string: str, attributes: attrs)
+	}
+
+	func description(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+		let str = "When you have posts, you'll see them here."
+		let attrs = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body)]
+		return NSAttributedString(string: str, attributes: attrs)
+	}
+
+	func backgroundColor(forEmptyDataSet scrollView: UIScrollView!) -> UIColor! {
+		return UIColor.init(red: 0.837, green: 0.837, blue: 0.837, alpha: 1)
 	}
 }
