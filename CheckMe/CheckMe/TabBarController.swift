34bca1a1bdfa9dc6acbc9dca922936abf0d375cf
Vadim Sosnovsky <vadzim.sasnouski@zorachka.com>
2022-06-28 12:59:44 +0300

    move tabBatItem configure from storyboard to code

--- CheckMe/CheckMe/TabBarController.swift
+++ CheckMe/CheckMe/TabBarController.swift
@@ -16,6 +16,7 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
 
         setupChildVC()
+        setTabBarAppearance()
     }
 
     private func setupChildVC() {
@@ -33,19 +34,58 @@ class TabBarController: UITabBarController {
             switch childVC {
             case let viewController as MainTableViewController:
                 // TODO: - build main midule
-                break
+                generateTabBarItems(viewController: viewController,
+                                    title: "Home",
+                                    image: UIImage(named: "Home_page"))
             case let viewController as CreateStoryViewController:
                 // TODO: - build story module
-                break
+                generateTabBarItems(viewController: viewController,
+                                    title: "Add story",
+                                    image: UIImage(named: "Add_page"))
             case let viewController as FavoritesViewController:
                 // TODO: - build favorities module
-                break
+                generateTabBarItems(viewController: viewController,
+                                    title: "Favorites",
+                                    image: UIImage(named: "Like_page"))
             case let viewController as SettingsTableViewController:
                 builder?.configureSettingsModule(viewController: viewController)
-                viewController.tabBarItem.image = UIImage(named: "Settings_page")
+                generateTabBarItems(viewController: viewController,
+                                    title: "Settings",
+                                    image: UIImage(named: "Settings_page"))
             default:
                 break
             }
         }
+    }
+
+    private func generateTabBarItems(viewController: UIViewController, title: String, image: UIImage?) {
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+    }
+
+    private func setTabBarAppearance() {
+        let xPosition: CGFloat = 10
+        let yPosition: CGFloat = 10
+        let width = tabBar.bounds.width - xPosition * 2
+        let height = tabBar.bounds.height + yPosition * 2
+
+        let roundLayer = CAShapeLayer()
+
+        let bezierPath = UIBezierPath(roundedRect: CGRect(x: xPosition,
+                                                          y: tabBar.bounds.minY - yPosition,
+                                                          width: width,
+                                                          height: height),
+                                      cornerRadius: height / 2)
+
+        roundLayer.path = bezierPath.cgPath
+        
+        tabBar.layer.insertSublayer(roundLayer, at: 0)
+
+        tabBar.itemWidth = width / 5
+        tabBar.itemPositioning = .centered
+
+        roundLayer.fillColor = UIColor.black.cgColor
+        tabBar.tintColor = .tabBatItemAccent
+        tabBar.unselectedItemTintColor = .white
     }
 }
